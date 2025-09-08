25a2b9da711a24746935b52c7640718a527fc5b5
AkhilPundir156 <akhilpundir156@gmail.com>
2025-09-08 23:48:09 +0530

    [ADD] big_brain_frontend:  Add home page for app
    This commit ensure minimal homepage, user demo data, and tooltip component.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,97 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AppDispatch, RootState } from "../store";
+import { setNavbarItem } from "../slices/uiSlice";
+
+import { people } from "../demo/People_demo";
+
+import { Button } from "../ui/ButtonElement";
+import ToolTip from "../ui/AnimatedTooltip";
+
+import HomeHeroImg from "../assets/img/HomeHero.png";
+import HomeHeroWhiteImg from "../assets/img/HomeHeroWhite.png";
 
 const Home = () => {
+    const navigation = useNavigate();
+    const theme = useSelector((state: RootState) => state.ui);
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        dispatch(setNavbarItem("home"));
+        return () => {
+            dispatch(setNavbarItem(null));
+        };
+    }, []);
+
     return (
-        <div></div>
+        <div className="w-full mt-[7%] flex">
+            {/* Left Section */}
+            <div className="w-1/2 p-[2%]">
+                <div className="w-fit pl-[20%] pt-[5%]">
+                    {/* Home Page Text Content */}
+                    <h2 className="text-[2.6rem] font-bold leading-snug">
+                        Meet Your{" "}
+                        <span className="text-primary-text">
+                            Smartest Brain Ever
+                        </span>{" "}
+                        !!!
+                    </h2>
+                    <p className="mt-4 text-lg tracking-wide text-tertiary-text">
+                        Stop juggling tabs and apps! Effortlessly chat with your{" "}
+                        <span className="font-semibold">
+                            Docs, Instagram, Twitter, YouTube, Files, and
+                            Content
+                        </span>
+                        . Summarize, search, and interact with your knowledge
+                        world—all in one place.
+                        <br />
+                        <br />
+                        Save anything to your Brain, then let AI (powered by{" "}
+                        <span className="font-semibold">Gemini-2.5</span>)
+                        organize, explain, and supercharge it for you.
+                    </p>
+
+                    {/* Action Buttons */}
+                    <div className="mt-8 flex gap-4">
+                        <Button
+                            variant="primary"
+                            size="lg"
+                            text="🚀 Get Started"
+                            isFull={false}
+                            onClickHandler={() => navigation("/my-brain")}
+                        />
+                        <Button
+                            variant="secondary"
+                            size="lg"
+                            text="📖 How to Use"
+                            isFull={false}
+                            onClickHandler={() => navigation("/how-to-use")}
+                        />
+                    </div>
+
+                    {/* Animated Tool Tip */}
+                    <div className="m-auto pt-24 flex w-full justify-center">
+                        <ToolTip items={people} />
+                    </div>
+                    <div className="ml-3 pt-3 text-center text-[1.2rem] font-[500] text-secondary-text">
+                        10+ users built their Brain Smart !!!
+                    </div>
+                </div>
+            </div>
+
+            {/* Right Section - Hero Image */}
+            <div className="w-1/2 flex justify-center">
+                <img
+                    src={
+                        theme.theme === "dark" ? HomeHeroImg : HomeHeroWhiteImg
+                    }
+                    alt="AI Powered Brain"
+                    className="w-[480px]"
+                />
+            </div>
+        </div>
     );
 };
 
---
+++ src/demo/People_demo.ts
@@ -0,0 +1,32 @@
+export const people = [
+    {
+        id: 1,
+        name: "Abhishek Jha",
+        designation: "Software Engineer",
+        image: "https://res.cloudinary.com/dye1lhutq/image/upload/v1757352593/jha_img_yiomfl.png",
+    },
+    {
+        id: 2,
+        name: "Aman Kumar",
+        designation: "System Engineer",
+        image: "https://res.cloudinary.com/dye1lhutq/image/upload/v1757352594/aman_img_ffcydr.png",
+    },
+    {
+        id: 3,
+        name: "Rehan Rao",
+        designation: "Data Scientist",
+        image: "https://res.cloudinary.com/dye1lhutq/image/upload/v1757352594/rehan_img_mslkkd.png",
+    },
+    {
+        id: 4,
+        name: "Abhishek Bhatt",
+        designation: "Service Now Developer",
+        image: "https://res.cloudinary.com/dye1lhutq/image/upload/v1757352592/bhatt_img_val0yl.png",
+    },
+    {
+        id: 5,
+        name: "Shivam Pundir",
+        designation: "Flutter Developer",
+        image: "https://res.cloudinary.com/dye1lhutq/image/upload/v1757352597/shivam_img_jec0nd.png",
+    },
+];
