894e4053750700cd7977896dbd8f6d8b7d736423
AkhilPundir156 <akhilpundir156@gmail.com>
2025-09-11 01:56:43 +0530

    [FIX] big_brain_frontend:  chat with brain component
    - Fix the complete styling of the component.
    - Add Keyboard event to handle enter and shift+enter.
    - Fix the resizing of input.
    - Add auto focus to input when component mounts.

--- src/components/BrainComponent/ChatWithbrain.tsx
+++ src/components/BrainComponent/ChatWithbrain.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { KeyboardEvent, useRef, useState, useEffect } from "react";
 
 import BrainService from "../../utils/brainService";
 
@@ -16,6 +16,24 @@ const ChatWithBrain = () => {
 
     const [messages, setMessages] = useState<Message[]>([]);
     const [isThinking, setIsThinking] = useState(false);
+
+    useEffect(() => {
+        const textarea = inputRef.current;
+        if (!textarea) return;
+
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                handleSendMessage();
+            }
+        };
+        //@ts-ignore
+        textarea.addEventListener("keydown", handleKeyDown);
+        return () => {
+            //@ts-ignore
+            textarea.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
 
     useEffect(() => {
         chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -45,23 +63,23 @@ const ChatWithBrain = () => {
     };
 
     return (
-        <div className="flex flex-col h-[600px] w-[750px] max-w-full border border-primary-border rounded-2xl bg-first-bg shadow-2xl">
-            <div className="flex-1 overflow-y-auto space-y-2 py-5 px-2">
+        <div className="flex flex-col h-[600px] w-[750px] max-w-full border border-primary-dot rounded-2xl bg-first-bg shadow-2xl">
+            <div className="flex-1 overflow-y-auto space-y-2 py-5 px-2 scrollbar-hide">
                 {messages.map((msg, index) => (
                     <div
                         key={index}
-                        className={`px-3 py-3 rounded-2xl shadow-sm max-w-[75%] break-words text-[15px] tracking-[-.2px] leading-[150%] ${
-                            msg.sender === "user"
-                                ? "bg-tertiary-bg text-secondary-text self-end ml-auto mr-3"
-                                : "bg-tertiary-bg text-secondary-text self-start ml-3"
-                        }`}
+                        className={`relative px-3 py-3 rounded-2xl shadow-sm w-fit max-w-[65%] break-words text-[15px] tracking-[-.2px] leading-[150%]
+                      ${
+                          msg.sender === "user"
+                              ? "bg-tertiary-bg text-secondary-text self-end ml-auto mr-3 after:content-[''] after:absolute after:top-0 after:right-[-6px] after:border-[14px] after:border-b-0 after:border-transparent after:border-t-tertiary-bg"
+                              : "bg-tertiary-bg text-secondary-text self-start ml-3 after:content-[''] after:absolute after:top-0 after:left-[-6px] after:border-[14px] after:border-b-0 after:border-transparent after:border-t-tertiary-bg"
+                      }`}
                     >
                         {msg.text}
                     </div>
                 ))}
-
                 {isThinking && (
-                    <div className="px-4 py-2 rounded-2xl bg-secondary-bg text-tertiary-text italic shadow-sm self-start ml-3 max-w-[60%]">
+                    <div className="px-4 py-2 rounded-2xl bg-tertiary-bg text-tertiary-text italic shadow-sm self-start ml-3 max-w-[65%]">
                         Brain is thinking...
                     </div>
                 )}
@@ -69,21 +87,30 @@ const ChatWithBrain = () => {
                 <div ref={chatEndRef} />
             </div>
 
-            <div className="flex gap-3 p-4 border-t border-secondary-border bg-first-bg rounded-b-2xl items-center">
+            <div className="flex gap-3 p-4 border-t border-secondary-border bg-first-bg rounded-b-2xl items-end">
                 <InputElement
-                    type="text"
+                    type="textarea"
                     placeholder="Type your message..."
                     inputReference={inputRef}
-                    addedClass="flex-1 items-center bg-secondary-bg rounded-xl text-base"
+                    addedClass="flex-1 items-center overflow-y-auto"
+                    onchange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                        const target = e.target;
+                        target.style.height = "auto";
+                        target.style.height =
+                            Math.min(target.scrollHeight, 128) + "px";
+                    }}
+                    row={1}
                 />
-                <Button
-                    variant="primary"
-                    size="md"
-                    text={isThinking ? "Thinking..." : "Send"}
-                    isFull={false}
-                    disbale={isThinking}
-                    onClickHandler={handleSendMessage}
-                />
+                <div className="pb-0.5">
+                    <Button
+                        variant="primary"
+                        size="md"
+                        text={isThinking ? "Thinking..." : "Send"}
+                        isFull={false}
+                        disbale={isThinking}
+                        onClickHandler={handleSendMessage}
+                    />
+                </div>
             </div>
         </div>
     );
